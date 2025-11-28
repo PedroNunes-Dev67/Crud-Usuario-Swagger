95481978ec5e25ffdccc76b745eb355a1159b3d5
Pedro Vitor Nunes Silva <pedrovitornunes89@gmail.com>
2025-11-27 23:23:56 -0300

    Implementando openApi/Swagger para documentar

--- src/main/java/Dio_spring/controller/UsuarioController.java
+++ src/main/java/Dio_spring/controller/UsuarioController.java
@@ -6,9 +6,12 @@ import Dio_spring.dto.UsuarioDtoRequest;
 import Dio_spring.dto.UsuarioDtoResponse;
 import Dio_spring.model.Usuario;
 import Dio_spring.service.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -18,30 +21,52 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("/usuarios")
+@RequestMapping(value = "/usuarios")
+@Tag(name = "Crud", description = "Crud de usuários em Java Springboot")
 public class UsuarioController {
 
     @Autowired
     private UsuarioService usuarioService;
 
-    @GetMapping("/{id}")
+    @Operation(summary = "Busca usuários por id", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuário encontrado"),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado")
+    })
+    @GetMapping(value = "/{id}")
     public ResponseEntity<Usuario> getUser(@PathVariable("id") Long id){
         return ResponseEntity.ok(usuarioService.getUser(id));
     }
 
+    @Operation(summary = "Busca lista de usuários cadastrados", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuários encontrados"),
+            @ApiResponse(responseCode = "400", description = "Lista vazia")
+    })
     @GetMapping
     public ResponseEntity<List<Usuario>> findALl(){
         List<Usuario> allUsers = usuarioService.findAll();
         return ResponseEntity.ok(allUsers);
     }
 
+    @Operation(summary = "Deleta os usuários pelo id", method = "DELETE")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuário encontrado e deletado"),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado")
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable("id") Long id){
         usuarioService.delete(id);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "Cadastra usuários", method = "POST")
     @PostMapping
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Usuário cadastrado"),
+            @ApiResponse(responseCode = "400", description = "Validation usado"),
+            @ApiResponse(responseCode = "409", description = "Conflito , usuário já cadastrado")
+    })
     public ResponseEntity<UsuarioDtoResponse> addUser(@RequestBody @Valid UsuarioDtoRequest usuarioDtoRequest){
 
         UsuarioDtoResponse usuarioDtoResponse = usuarioService.addUser(usuarioDtoRequest);
@@ -53,6 +78,12 @@ public class UsuarioController {
         return ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(id).toUri();
     }
 
+    @Operation(summary = "Atualiza senha do usuário",method = "PUT")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuário atualizado"),
+            @ApiResponse(responseCode = "400", description = "Validation usado"),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado")
+    })
     @PutMapping("/{id}")
     public ResponseEntity<UsuarioDtoResponse> updateUser(@PathVariable("id") Long id,@RequestBody @Valid RedefinirSenha senha){
         return ResponseEntity.ok(usuarioService.updateUser(id,senha));
